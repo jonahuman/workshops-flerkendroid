4db147ba6de410522ce54bb62f4678f243f466e0
pablofc <pablo.fernando.cargnelutti@gmail.com>
2023-11-09 20:15:41 -0300

    Initial commit

---
+++ src/test/java/StackTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class StackTest {
+
+  @Test
+  public void xyz() {
+    assertThat(true, is(true));
+  }
+}
